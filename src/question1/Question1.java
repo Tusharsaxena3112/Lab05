c236efada714ca9ccbdd731fe48f86fcd4c9f1b9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-09 14:44:55 +0530

    Initial commit

---
+++ src/question1/Question1.java
@@ -0,0 +1,15 @@
+package question1;
+import java.util.Scanner;
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        if ((a ^ b) == (a | b)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("Bye");
+        }
+    }
+}
