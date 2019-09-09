e3a02ba2e1d55bcb2acac320f930b20966774ef9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-09 20:02:57 +0530

    finally done

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -4,22 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question2 {
+    // Bottle neck Problem ----> Codevita
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int no = input.nextInt();
         int arr[] = new int[no];
         int arr1[] = new int[50];
-        for (int i = 0; i <= no - 1; i++) {
+        for (int i = 0; i < no; i++) {
             arr[i] = input.nextInt();
         }
-        Arrays.sort(arr);
-        int j = 0;
-        for (int i = 0; i <= no - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                arr[i] = 0;
-                arr1[j] = arr[i + 1];
-                j = j + 1;
+        for (int i = 0; i < no; i++) {
+            int c = 0;
+            for (int j = 0; j <= no - 1; j++) {
+                if (arr[i] == arr[j]) {
+                    c++;
+                }
+                arr1[i] = c;
             }
         }
+        Arrays.sort(arr1);
+        System.out.println(arr1[arr1.length - 1]);
     }
 }
+
